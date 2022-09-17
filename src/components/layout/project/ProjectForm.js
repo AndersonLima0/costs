43933410ca5dfa8c0a28c1f80b4792039e45dc0d
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2022-09-17 20:52:02 -0300

    Criação do formulario de projeto

---
+++ src/components/layout/project/ProjectForm.js
@@ -0,0 +1,21 @@
+function ProjectForm(){
+    return(
+        <form>
+            <div>
+                <input type="text" placeholder="Insira o nome do se projeto"/>
+            </div>
+            <div>
+                <input type="number" placeholder="Insira o orçamento total"/>
+            </div>
+            <div>
+                <select name="category_id">
+                    <option disabled>Selecione a categoria</option>
+                </select>
+            </div>
+            <div>
+                <input type="submit" value="Criar projeto"/>
+            </div>
+        </form>
+    )
+}
+export default ProjectForm;

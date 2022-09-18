39d5545433031a6681e1dfb7ac916669171bb171
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2022-09-17 21:33:11 -0300

    Estilização do form

--- src/pages/NewProject.js
+++ src/pages/NewProject.js
@@ -7,7 +7,7 @@ function NewProject(){
         <div className={styles.newproject_container}>
             <h1>Criar projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm/>
+            <ProjectForm btnText="Cria projeto"/>
         </div>
     )
 }
